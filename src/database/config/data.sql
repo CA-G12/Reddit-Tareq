124cd4d828168dab64ea3c1fbc1c60e504558cfd
unknown <tareqabuaqlain1@gmail.com>
2022-09-04 23:10:56 +0300

    create database schema config file related #4

--- src/database/config/data.sql
+++ src/database/config/data.sql
@@ -1,20 +1,30 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users , posts CASCADE;
+DROP TABLE IF EXISTS users,
+post,
+comment CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE posts (
+CREATE TABLE post (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content VARCHAR(255) NOT NULL,
-    usernameId int
-    foreign key (usernameId) references users(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comment (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES post(id) ON DELETE CASCADE,
+    content VARCHAR(255) NOT NULL
 );
 
 COMMIT;
