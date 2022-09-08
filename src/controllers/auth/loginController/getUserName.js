c08b74c8f5edcb991c1504ba69c30a1b8a4af39d
unknown <tareqabuaqlain1@gmail.com>
2022-09-08 21:04:45 +0300

    fix query bugs , fix getUsername , fix get one post by id , fix get all post related #33

--- src/controllers/auth/loginController/getUserName.js
+++ src/controllers/auth/loginController/getUserName.js
@@ -1,7 +1,7 @@
 const { getUserName } = require('../../../database/queries');
 
 const getTheUserName = async (req, res) => {
-  const { id } = req.body;
+  const { id } = req;
   try {
     const data = await getUserName(id);
     res.json(data.rows);
@@ -10,4 +10,4 @@ const getTheUserName = async (req, res) => {
   }
 };
 
-module.exports = {getTheUserName};
+module.exports = { getTheUserName };
--- src/controllers/postController/getPost.js
+++ src/controllers/postController/getPost.js
@@ -2,9 +2,19 @@ const { getPost } = require('../../database/queries');
 
 const getThePost = (req, res) => {
   const { id } = req.params;
+  const userId = req.id;
   getPost(id)
-    .then((data) => data)
-    .then((data) => res.json(data.rows))
+    .then((data) => {
+      if (data.rows.length) {
+        if (data.rows[0].user_id === userId) {
+          res.json(data.rows);
+        } else {
+          res.json({ message: 'Not Authorized' });
+        }
+      } else {
+        res.json({ message: ' Post Dose not exist !' });
+      }
+    })
     .catch((err) => {
       res.json(err);
     });
