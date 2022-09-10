adbedebdf32993930bd414b06a1e1b1b9ebc9a6d
unknown <tareqabuaqlain1@gmail.com>
2022-09-10 19:53:23 +0300

    deleted the console log that i use it to test related #44

--- src/controllers/postController/showPostPage.js
+++ src/controllers/postController/showPostPage.js
@@ -1,7 +1,4 @@
 const showPostPage = (req, res) => {
-  // console.log('location', location);
-  console.log('req.params.id', req.params.id);
-
   res.json({ location: './post/index.html', id: `${req.params.id}` });
 };
 
